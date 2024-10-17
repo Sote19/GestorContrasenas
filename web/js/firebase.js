f8f70d8c2a55a3a703d067c98d9e14e7a90ce134
Soteras <156694267+Sote19@users.noreply.github.com>
2024-10-17 19:06:07 +0200

    Add files via upload

--- web/js/firebase.js
+++ web/js/firebase.js
@@ -1,9 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js"; // Import Firestore
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCF6y0zxzW5YmVwqZeelTXiqtm5JXhC_Us",
   authDomain: "gtx-database.firebaseapp.com",
@@ -17,22 +19,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize Firestore
-const db = getFirestore(app); // Asegúrate de pasar `app` aquí
-
-// Función para registrar un nuevo usuario
-export async function registerUser(name, email, password) {
-    try {
-        // Agregar documento a la colección 'USUARIOS'
-        const docRef = await addDoc(collection(db, "USUARIOS"), {
-            nombre: name,
-            correo: email,
-            contraseña: password, // Considera cifrar la contraseña
-            fecha_registro: new Date()
-        });
-        console.log("Documento añadido con ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error añadiendo documento: ", e);
-    }
-}
