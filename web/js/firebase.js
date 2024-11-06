4c5e1f78852584e1e8c3224996aa2012b5be2286
Soteras <156694267+Sote19@users.noreply.github.com>
2024-11-06 17:07:54 +0100

    Update firebase.js

--- web/js/firebase.js
+++ web/js/firebase.js
@@ -1,48 +1,21 @@
-// Importar las funciones desde SDKs que se necesitan 
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configuración de firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCF6y0zxzW5YmVwqZeelTXiqtm5JXhC_Us",
-  authDomain: "gtx-database.firebaseapp.com",
-  projectId: "gtx-database",
-  storageBucket: "gtx-database.appspot.com",
-  messagingSenderId: "804205977819",
-  appId: "1:804205977819:web:ddcd76134e6b4690a36cfe",
-  measurementId: "G-C6H1EMYN6H"
+  apiKey: "AIzaSyB_0yehy8ZaXcl3UY22PmlNpB7wIV4HpAs",
+  authDomain: "gestorgtx.firebaseapp.com",
+  projectId: "gestorgtx",
+  storageBucket: "gestorgtx.firebasestorage.app",
+  messagingSenderId: "155430917151",
+  appId: "1:155430917151:web:ecf5d9c6f086963654312a",
+  measurementId: "G-W04SS3Y4RP"
 };
 
-// Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Inicializar Firestore
-const db = getFirestore(app);
 const analytics = getAnalytics(app);
-
-// 5. Función para escribir datos en Firestore
-async function addData() {
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      name: "Ada Wong",
-      age: 30
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-}
-
-// 6. Función para leer datos de Firestore
-async function getData() {
-  const querySnapshot = await getDocs(collection(db, "users"));
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
-  });
-}
-
-// 7. Llamar a las funciones
-addData();  // Escribir datos
-getData();  // Leer datos
