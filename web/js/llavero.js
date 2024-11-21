e823d895f687fd598ccd75b091fc6bb88bcab08a
X4VICO <156704558+X4VICO@users.noreply.github.com>
2024-11-21 18:37:24 +0100

    Add files via upload

--- web/js/llavero.js
+++ web/js/llavero.js
@@ -2,8 +2,9 @@ import { auth } from './firebase.js';
 import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 
+
+// ---------------------VERIFICACIÓN DE USUARIO REGISTRADO---------------------
 window.onload = function() {
-  // Monitoreamos el estado de autenticación del usuario
   onAuthStateChanged(auth, (user) => {
     if (user) {
       // Si el usuario está autenticado, mostramos su nombre
@@ -14,18 +15,17 @@ window.onload = function() {
       window.location.href = 'iniciosesion.html';
     }
   });
-
-  // Botón de cerrar sesión
-  document.getElementById('logout-button').addEventListener('click', async () => {
-    try {
-      // Cerrar sesión de Firebase
-      await signOut(auth);
-
-      // Redirigir al usuario al índice después de cerrar sesión
-      window.location.href = 'index.html';
-    } catch (error) {
-      console.error("Error al cerrar sesión:", error);
-      alert("No se pudo cerrar sesión. Inténtalo de nuevo.");
-    }
-  });
 };
+
+
+// ---------------------------------CERRAR SESIÓN--------------------
+document.getElementById('logout-button').addEventListener('click', async () => {
+  try {
+    // Cerrar sesión de Firebase
+    await signOut(auth);    // Redirigir al usuario al índice después de cerrar sesión
+    window.location.href = 'index.html';
+  } catch (error) {
+    console.error("Error al cerrar sesión:", error);
+    alert("No se pudo cerrar sesión. Inténtalo de nuevo.");
+  }
+});
